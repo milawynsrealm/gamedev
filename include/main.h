b5c2fcd6b7af7f0a44f033a1f0b368874b035dbb
Lee Schroeder <spaceseel@gmail.com>
2018-01-18 12:08:41 -0700

    This library is designed to be a cross-platform entry point for applications. At the moment, it only supports Windows 2000+ and POSIX-based operating systems like GNU/Linux, but more may be added as time goes on. Also, it hasn't yet been formally tested, so if any problems come up, then please let me know.

---
+++ include/main.h
@@ -0,0 +1,62 @@
+/*
+	Copyright (c) 2018, Lee Schroeder
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+		* Redistributions of source code must retain the above copyright
+		  notice, this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright
+		  notice, this list of conditions and the following disclaimer in the
+		  documentation and/or other materials provided with the distribution.
+		* Neither the name of the <organization> nor the
+		  names of its contributors may be used to endorse or promote products
+		  derived from this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL LEE SCHROEDER BE LIABLE FOR ANY
+	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/*
+	NOTES:
+	* wWinMain() is the UNICODE version of the main program entry for Windows.
+	* Windows uses 'wchar_t' while other operating systems like GNU/Linux uses
+	  'char' for parsing the command-line arguments. It is up to the developer(s)
+	  to deal with both cases.
+	* This library requires Windows 2000 or later, although only Windows XP or later
+	  will be officially supported.
+*/
+#ifdef(_WIN32)
+#include <windef.h>
+#include <shellapi.h>
+#include <wchar.h>
+#else
+typedef WCHAR char;
+#endif /* _WIN32 */
+
+extern int ProgramMain(int argc, WCHAR *argv[]);
+
+#ifdef(_WIN32)
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	int argc;
+	WCHAR** argv;
+
+	/* Converts the command line into a usable format */
+	argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+	return ProgramMain(argc, argv[]);
+}
+#else
+int main(int argc, char *argv[])
+{
+	return ProgramMain(argc, argv);
+}
+#endif
